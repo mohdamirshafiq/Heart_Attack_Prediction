7666320c45f2a4e981d5e76165fe6cddb0f43ad1
Mohd Amir Shafiq Mohd Hamzah <amirakira92@gmail.com>
2022-06-21 20:53:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,17 +22,13 @@ with st.form("Patient's info: "):
     st.write('Hit the information boxes to know more to know wether you have the possibility or not?')
     
     age = st.number_input('Key in your age')
-    # sex = st.number_input('Key in sex, Female:0,Male:1')
-    cp = st.number_input('Key in chest pain type (cp), 0:typical angina,1:atypical angina,2:non-anginal pain,3:asyptomatic')
-    # trtbps = st.number_input('Key in resting blood pressure(trtbps)')
-    # chol = st.number_input('Key in cholestrol readings')
-    # fbs = st.number_input('Key in fasting blood sugar(fbs),0:False,1:True')
-    # restecg = st.number_input('Key in resting electrocardiographic(restecg), 0:normal,1:ST-T wave abnormality,2:probable ventricular hypertrophy')
-    thalachh = st.number_input('Key in maximum heart rate achieved(thalachh)')
-    # exng = st.number_input('Key in exercise induced angina value(exng), 0:no,1:yes')
-    oldpeak = st.number_input('Key in oldpeak')
-    # slp = st.number_input('Key in slp')
-    # caa = st.number_input('Key in caa')
+    
+    cp = st.number_input('Key in chest pain type (cp), 0:typical angina, 1:atypical angina, 2:non-anginal pain, 3:asyptomatic')
+    
+    thalachh = st.number_input('Key in maximum heart rate achieved (thalachh)')
+    
+    oldpeak = st.number_input('Key in Oldpeak')
+    
     thall = st.number_input('Key in thall')
     
     submitted = st.form_submit_button("Submit")
@@ -42,37 +38,3 @@ with st.form("Patient's info: "):
         outcome_dict = {0: 'Less chance of heart attack',
                         1: 'More chance of heart attack'}
         st.write(outcome_dict[outcome[0]])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
